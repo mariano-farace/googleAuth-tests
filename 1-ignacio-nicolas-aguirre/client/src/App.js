d50d21c198e32b6ea8dd6837c4a01f3f1fab6345
Mariano <mariano_farace@hotmail.com>
2022-03-06 20:19:34 +0100

    First commit

---
+++ 1-ignacio-nicolas-aguirre/client/src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+
+import React, { useState, useEffect } from "react";
+import { getMyGoogleCalendarsList } from "./calendarApi";
+import { setTokens } from "./tokens";
+
+function App() {
+  useEffect(() => {
+    handleTokenFromQueryParams();
+  }, []);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const createGoogleAuthLink = async () => {
+    try {
+      const request = await fetch("http://localhost:8080/createAuthLink", {
+        method: "POST",
+      });
+      const response = await request.json();
+      window.location.href = response.url;
+    } catch (error) {
+      console.log("App.js 12 | error", error);
+      throw new Error("Issue with Login", error.message);
+    }
+  };
+
+  const handleTokenFromQueryParams = () => {
+    const query = new URLSearchParams(window.location.search);
+    const accessToken = query.get("accessToken");
+    const refreshToken = query.get("refreshToken");
+    const expirationDate = newExpirationDate();
+    console.log("App.js 30 | expiration Date", expirationDate);
+    if (accessToken && refreshToken) {
+      storeTokenData(accessToken, refreshToken, expirationDate);
+      setIsLoggedIn(true);
+    }
+  };
+
+  const newExpirationDate = () => {
+    var expiration = new Date();
+    expiration.setHours(expiration.getHours() + 1);
+    return expiration;
+  };
+
+  const storeTokenData = async (token, refreshToken, expirationDate) => {
+    sessionStorage.setItem("accessToken", token);
+    sessionStorage.setItem("refreshToken", refreshToken);
+    sessionStorage.setItem("expirationDate", expirationDate);
+  };
+
+  const signOut = () => {
+    setIsLoggedIn(false);
+    sessionStorage.clear();
+  };
+
+  return (
+    <div className="App">
+      <h1>Google</h1>
+      {!isLoggedIn ? (
+        <button onClick={createGoogleAuthLink}>Login</button>
+      ) : (
+        <>
+          <button onClick={getMyGoogleCalendarsList}>
+            Get Google Calendars
+          </button>
+          <button onClick={signOut}>Sign Out</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # googleAuth-tests
+
+https://levelup.gitconnected.com/handling-access-tokens-for-google-apis-with-react-node-js-tutorial-5ebf94d8f90f
+https://dev.to/shaan_alam/how-to-implement-google-authentication-in-your-react-applications-jb6
+https://notangad.medium.com/react-google-login-and-node-js-implementation-32002d8d8d78
+https://blog.prototypr.io/how-to-build-google-login-into-a-react-app-and-node-express-api-821d049ee670
+https://gist.github.com/jhackett1/ff383ca58d69c8a8d9285e81fc065c08
+
+https://github.com/topics/googleauth
+https://github.com/topics/react-google-login
